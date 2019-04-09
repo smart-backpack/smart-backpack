34f9b88141e5f5dcb0a6972b56bbc0dd6bc5a2a4
akhilamudela <48960525+akhilamudela@users.noreply.github.com>
2019-04-08 23:39:45 -0400

    Update MFRC522.h

--- MFRC522.h
+++ MFRC522.h
@@ -1,16 +1,4 @@
-/**
- * MFRC522.h - Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT.
- * Based on code Dr.Leong   ( WWW.B2CQSHOP.COM )
- * Created by Miguel Balboa (circuitito.com), Jan, 2012.
- * Rewritten by Søren Thing Andersen (access.thing.dk), fall of 2013 (Translation to English, refactored, comments, anti collision, cascade levels.)
- * Extended by Tom Clement with functionality to write to sector 0 of UID changeable Mifare cards.
- * Released into the public domain.
- * 
 
-
--- Repurposed to fit Raspberry Pi --- 
-
- */
 #ifndef MFRC522_h
 #define MFRC522_h
 
